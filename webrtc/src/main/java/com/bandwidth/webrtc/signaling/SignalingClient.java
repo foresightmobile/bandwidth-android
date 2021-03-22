b10020db75f6f75badc83b02320e39d9615dc75e
Mike Hamer <mhamer@bandwidth.com>
2021-03-22 13:36:36 -0400

    Add implementation methods

--- webrtc/src/main/java/com/bandwidth/webrtc/signaling/SignalingClient.java
+++ webrtc/src/main/java/com/bandwidth/webrtc/signaling/SignalingClient.java
@@ -1,5 +1,35 @@
 package com.bandwidth.webrtc.signaling;
 
+import com.bandwidth.webrtc.signaling.listeners.OnConnectListener;
+import com.bandwidth.webrtc.signaling.listeners.OnDisconnectListener;
+import com.bandwidth.webrtc.signaling.listeners.OnOfferSdpListener;
+import com.bandwidth.webrtc.signaling.listeners.OnRequestToPublishListener;
+import com.bandwidth.webrtc.signaling.listeners.OnSetMediaPreferencesListener;
+
 public class SignalingClient implements Signaling {
 
+    @Override
+    public void setOnConnectListener(OnConnectListener listener) {
+        
+    }
+
+    @Override
+    public void setOnDisconnectListener(OnDisconnectListener listener) {
+
+    }
+
+    @Override
+    public void setOnOfferSdpListener(OnOfferSdpListener listener) {
+
+    }
+
+    @Override
+    public void setOnRequestToPublishListener(OnRequestToPublishListener listener) {
+
+    }
+
+    @Override
+    public void setOnSetMediaPreferencesListener(OnSetMediaPreferencesListener listener) {
+
+    }
 }
